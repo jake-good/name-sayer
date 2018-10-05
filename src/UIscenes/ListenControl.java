e8c6b7907fc32c0314a70d2384aa69fa04b3cc42
jake-good <jgoo479@aucklanduni.ac.nz>
2018-10-05 23:07:49 +1300

    Changes to styling for UI

--- src/UIscenes/ListenControl.java
+++ src/UIscenes/ListenControl.java
@@ -4,9 +4,11 @@ import javafx.scene.control.Button;
 
 public class ListenControl {
 
-    public Button _recordButtom;
-
     public void Record() {
         new sceneChange("RECORD");
+    }
+
+    public void Select() {
+        new sceneChange("SELECT");
     }
 }
--- src/UIscenes/RecordControl.java
+++ src/UIscenes/RecordControl.java
@@ -5,9 +5,21 @@ import javafx.scene.control.Button;
 
 public class RecordControl {
 
-    public Button _compare;
+    // The Record scene will presumably have a name and a name file associated with it. In this class we need individual methods for the following:
+    // Playing the original name
+    // Recording a new name
+    // Playing/Pausing that recording
+    // Viewing/deleting old recordings.
 
     public void Compare() {
         new sceneChange("ASSESS");
+    }
+
+    public void Select() {
+        new sceneChange("SELECT");
+    }
+
+    public void Listen() {
+        new sceneChange("LISTEN");
     }
 }

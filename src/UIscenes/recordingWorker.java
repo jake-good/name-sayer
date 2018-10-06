3ee387c2a07d5a52e0170457ddf74f83088ad28c
jake-good <jake.good1699@gmail.com>
2018-10-06 16:19:05 +1300

    Merge branch 'master' of /home/jake/IdeaProjects/name-sayer with conflicts.

---
+++ src/UIscenes/recordingWorker.java
@@ -0,0 +1,47 @@
+package NameSayerGUI;
+
+import javafx.application.Platform;
+
+import javax.swing.*;
+
+public class recordingWorker extends SwingWorker<Void,Void> {
+
+    private String _currentName;
+    private int _fileCount;
+    private nameView _view;
+
+    public recordingWorker(String currentName, int fileCount, nameView view){
+        _currentName = currentName;
+        _fileCount = fileCount;
+        _view = view;
+    }
+
+    @Override
+    public Void doInBackground(){
+
+        //Records the attempt at saying the name.
+        String cmd = "ffmpeg -f alsa -i default  -t 3 'names/" + _currentName +
+                "/attempt" + _fileCount + "_" + _currentName +".wav'";
+        ProcessBuilder builder = new ProcessBuilder("bash", "-c",
+                cmd);
+        try{
+            Process process = builder.start();
+            process.waitFor();
+
+        }catch (Exception e){
+
+        }
+        return null;
+    }
+
+    @Override
+    public void done(){
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                _view.getAttempts();
+                _view.setDone();
+            }
+        });
+    }
+}

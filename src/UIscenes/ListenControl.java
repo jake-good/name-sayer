ee7e684548a8fb5f64e5711d0e106508b87772c6
jake-good <jgoo479@aucklanduni.ac.nz>
2018-10-06 14:35:20 +1300

    Added names file and UI changes

--- src/UIscenes/ListenControl.java
+++ src/UIscenes/ListenControl.java
@@ -1,8 +1,17 @@
 package UIscenes;
 
-import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
-public class ListenControl {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ListenControl implements Initializable {
+
+    private NameModel _currentName;
+    private int _nameIndex;
+
+    public Label _nameLabel;
 
     public void Record() {
         new sceneChange("RECORD");
@@ -11,4 +20,34 @@ public class ListenControl {
     public void Select() {
         new sceneChange("SELECT");
     }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        _nameIndex = 0;
+        setName();
+    }
+
+    public void setName() {
+        _currentName = NameModel._Names.get(_nameIndex);
+        _nameLabel.setText(_currentName.getName());
+    }
+
+    public void next() {
+        if (_nameIndex < NameModel._Names.size() - 1) {
+            _nameIndex++;
+            setName();
+        }
+    }
+
+    public void previous() {
+        if (_nameIndex > 0) {
+            _nameIndex--;
+            setName();
+        }
+    }
+
+    public void play() {
+        // Play the current name via the filename
+    }
+
 }
--- src/UIscenes/SelectControl.java
+++ src/UIscenes/SelectControl.java
@@ -1,16 +1,32 @@
 package UIscenes;
 
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class SelectControl {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SelectControl implements Initializable {
 
     public Button _listenButton;
     public Label _uploadList;
-
+    public TextField _firstName;
+    public TextField _familyName;
+    private Boolean List;
 
     public void Listen() {
-        new sceneChange("LISTEN");
+        if (!List) {
+            String fullName = _firstName.getText() + " " +  _familyName.getText();
+            new NameModel(fullName, "placeholder");
+            new sceneChange("LISTEN");
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        List = false;
     }
 }
